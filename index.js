a5a34b62b9b756fcd7a383765340dde4778784d0
legendarybladerofmilkywaygalaxy <mudunurisriram@gmail.com>
2017-06-24 04:56:06 +0530

    pics

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ app.post('/webhook/', function(req, res) {
 			decideMessage(sender, text)
 
 		}
+		if (event.postback) {
+			let text = JSON.stringify(event.postback)
+			decideMessage(sender, text)
+			continue
+		}
 	}
 	res.sendStatus(200)
 })
@@ -94,17 +99,103 @@ function decideMessage(sender, text1){
         sendText(sender, "I cant play i am a bot")
 	} else if (text.includes("sare")){
         sendText(sender, ":)")
+	} else if (text.includes("pyramid")){
+        sendImageMessage(sender, "https://en.wikipedia.org/wiki/New7Wonders_of_the_World#/media/File:Kheops-Pyramid.jpg")
+    } else if (text.includes("tajmahal")){
+        sendImageMessage(sender, "https://en.wikipedia.org/wiki/New7Wonders_of_the_World#/media/File:Taj_Mahal_in_March_2004.jpg") 
+    } else if (text.includes("chaina")){
+        sendImageMessage(sender, "https://en.wikipedia.org/wiki/New7Wonders_of_the_World#/media/File:The_Great_Wall_of_China_at_Jinshanling.jpg")
+    } else if (text.includes("petra")){
+        sendImageMessage(sender, "https://en.wikipedia.org/wiki/New7Wonders_of_the_World#/media/File:Petra_Jordan_BW_21.JPG")
+    } else if (text.includes("colloseum")){
+        sendImageMessage(sender, "https://en.wikipedia.org/wiki/New7Wonders_of_the_World#/media/File:Colosseum_in_Rome,_Italy_-_April_2007.jpg")
+    } else if (text.includes("machupichhu")){
+        sendImageMessage(sender, "https://en.wikipedia.org/wiki/New7Wonders_of_the_World#/media/File:Machu_Picchu,_Peru.jpg")
+    } else if (text.includes("christ")){
+        sendImageMessage(sender, "https://en.wikipedia.org/wiki/New7Wonders_of_the_World#/media/File:1_cristor_redentor_2014.jpg")                    
 	} else if (text.includes("pappa")){
         sendText(sender, "pappa")
 	} else if (text.includes("hello")){
         sendText(sender, "hello")
 	} else {
-		sendText(sender, "sorry, Sriram is so lazy that he didnt programmed me completely ask him to completely program me. Note: please dont use capitals use only small letters")
+		sendText(sender, "sorry, Sriram is so lazy that he didnt programmed me completely ask him to completely program me.")
+		sendButtonMessage(sender, "Which is favourite wonder in the world?")
 	}
 }
 
+
 function sendText(sender, text) {
 	let messageData = {text: text}
+	sendRequest(sender, messageData)
+}
+
+function sendButtonMessage(sender, text){
+	let messageData = {
+		"attachment":{
+        "type":"template",
+        "payload":{
+        "template_type":"button",
+        "text":text,
+        "buttons":[
+          {
+            "type":"postback",
+            "title":"Great Pyramid Of Giza",
+            "Payload":"pyramid"
+          },
+          {
+            "type":"postback",
+            "title":"Taj Mahal",
+            "Payload":"tajmahal"
+          },
+          {
+            "type":"postback",
+            "title":"Great Wall Of Chaina",
+            "Payload":"chaina"
+          },
+          {
+            "type":"postback",
+            "title":"Petra",
+            "Payload":"petra"
+          },
+          {
+            "type":"postback",
+            "title":"The Colloseum",
+            "Payload":"colloseum"
+          },
+          {
+            "type":"postback",
+            "title":"Machu Pichhu",
+            "Payload":"machupichhu"
+          },
+          {
+            "type":"postback",
+            "title":"Christ The Redeemer",
+            "Payload":"christ"
+          },
+          {
+            "type":"postback",
+            "title":"Start Chatting",
+            "payload":"USER_DEFINED_PAYLOAD"
+          }
+        ]
+      }
+    }
+	}
+	sendRequest(sender, messageData)
+}
+
+function sendImageMessage(sender, imageUrl) {
+	let imageData = {
+		"attachment":{
+        "type":"image",
+        "payload":{
+        "url":imageUrl
+      }
+    }
+	}
+	sendRequest(sender, messageData)
+}
+function sendRequest(sender, messageData) {
 	request({
 		url: "https://graph.facebook.com/v2.6/me/messages",
 		qs : {access_token : token},
@@ -121,8 +212,6 @@ function sendText(sender, text) {
 		}
 	})
 }
-
-
 app.listen(app.get('port'), function() {
 	console.log("running: port")
 })
