9a7b8d7e200d8f7768131607994031644f2db7a7
legendarybladerofmilkywaygalaxy <mudunurisriram@gmail.com>
2017-06-23 21:30:30 +0530

    sriram busy

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ app.post('/webhook/', function(req, res) {
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
-			sendText(sender, " " + text.substring(0, 100))
+			//sendText(sender, " " + text.substring(0, 100))
+			sendText(sender, "Hii sriram is busy now so created me to chat with you ")
+
 		}
 	}
 	res.sendStatus(200)
